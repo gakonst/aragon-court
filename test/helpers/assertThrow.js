1ef63fe2bcaf2ddee8e20b59f5265f65ef35d77b
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2019-09-19 16:42:56 -0300

    helpers: polish assert revert

--- test/helpers/assertThrow.js
+++ test/helpers/assertThrow.js
@@ -1,6 +1,6 @@
-const REVERT_CODE = 'revert'
-const THROW_ERROR_PREFIX_A = 'Returned error: VM Exception while processing transaction:'
-const THROW_ERROR_PREFIX_B = 'VM Exception while processing transaction:' // ganache-core
+const REVERT_ERROR_CODE = 'revert'
+const GANACHE_CLI_ERROR_PREFIX = 'Returned error: '
+const GANACHE_CORE_ERROR_PREFIX = 'VM Exception while processing transaction:'
 
 function assertError(error, expectedErrorCode) {
   assert(error.message.search(expectedErrorCode) > -1, `Expected error code "${expectedErrorCode}" but failed with "${error}" instead.`)
@@ -18,25 +18,15 @@ async function assertThrows(blockOrPromise, expectedErrorCode, expectedReason) {
 }
 
 async function assertRevert(blockOrPromise, reason) {
-  let ganacheCoreError = true;
-  const error = await assertThrows(blockOrPromise, REVERT_CODE, reason)
-  const errorPrefix_A = `${THROW_ERROR_PREFIX_A} ${REVERT_CODE}`
-  const errorPrefix_B = `${THROW_ERROR_PREFIX_B} ${REVERT_CODE}`
+  const error = await assertThrows(blockOrPromise, REVERT_ERROR_CODE, reason)
 
-  if (error.message.includes(errorPrefix_A)) {
-    ganacheCoreError = false;
-    error.reason = error.message.replace(errorPrefix_A, '')
+  error.reason = error.message
+    .replace(GANACHE_CLI_ERROR_PREFIX, '')
+    .replace(`${GANACHE_CORE_ERROR_PREFIX} ${REVERT_ERROR_CODE}`, '')
+    .replace(` -- Reason given: ${reason}.`, '').trim()
     // Truffle 5 sometimes add an extra ' -- Reason given: reason.' to the error message 🤷
-    error.reason = error.reason.replace(` -- Reason given: ${reason}.`, '').trim()
-  }
 
-  if (error.message.includes(errorPrefix_B) && ganacheCoreError){
-    error.reason = error.message.replace(errorPrefix_B, '')
-    // Truffle 5 sometimes add an extra ' -- Reason given: reason.' to the error message 🤷
-    error.reason = error.reason.replace(` -- Reason given: ${reason}.`, '').trim()
-  }
-
-  if (process.env.SOLIDITY_COVERAGE !== 'true' && reason) {
+  if (reason) {
     assert.equal(error.reason, reason, `Expected revert reason "${reason}" but failed with "${error.reason || 'no reason'}" instead.`)
   }
 }
